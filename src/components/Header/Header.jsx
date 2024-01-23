eabb717fe4f0bc99a6ec422bc64f1ccf699f59a4
Greg Tosato <gregtosato@gmail.com>
2024-01-23 12:27:13 +1100

    update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import styles from "./Header.module.scss";
-import logo from "../../assets/google_books_logo.png";
+import logo from "../../assets/google_books_logo2.png";
 
 const Header = ({ setSearchTerm }) => {
   const [userInput, setUserInput] = useState("");
